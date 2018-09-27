0f8648cea352e3709f51a398026610f47f23e943
Ikeda Hiroya <ikeda@mm.doshisha.ac.jp>
2018-09-27 23:33:39 +0900

    download

---
+++ categorize.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+from scipy.misc import toimage
+from keras.datasets import cifar10
+
+
+if __name__ == '__main__' :
+
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    print(X_train.shape, y_train.shape)
+    print(X_test.shape, y_test.shape)
+
+
+    nclasses = 10
+
+    pos = 1
+
+    for targetClass in range(nclasses) :
+        targetIdx = []
+
+        for i in rage(len(y_train)) :
+            if y_train[i][0] == targetClass :
+                targetIdx.append(i)
+
+        np.random.shuffle(targetIdx)
+
+        for idx in target[:10]:
+            img = toimage(X_train(idx))
+            plt.subplot(10, 10, pos)
+            plt.imshow(img)
+            plt.axis('off')
+            pos += 1
+
+    plt.show()
